2b255212f7ab99b9257551ca3eed1457c4414616
Octavian Patrascoiu <opatrascoiu@yahoo.com>
2022-11-24 13:08:29 +0000

    [#26] Finalize implementation of NopEventListener

--- src/jdmn/runtime/listener/NopEventListener.py
+++ src/jdmn/runtime/listener/NopEventListener.py
@@ -10,9 +10,32 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations under the License.
 #
+from typing import Any
+
 from jdmn.runtime.listener.EventListener import EventListener
+from jdmn.runtime.listener.Arguments import Arguments
+from jdmn.runtime.listener.DRGElement import DRGElement
+from jdmn.runtime.listener.Rule import Rule
 
 
 class NopEventListener(EventListener):
     def __init__(self):
         EventListener.__init__(self)
+
+    def startDRGElement(self, element: DRGElement, arguments: Arguments) -> None:
+        pass # nothing
+
+    def endDRGElement(self, element: DRGElement, arguments: Arguments, output: Any, duration: int) -> None:
+        pass
+
+    def startRule(self, element: DRGElement, rule: Rule) -> None:
+        pass
+
+    def matchRule(self, element: DRGElement, rule: Rule) -> None:
+        pass
+
+    def endRule(self, element: DRGElement, rule: Rule, result: Any) -> None:
+        pass
+
+    def matchColumn(self, rule: Rule, columnIndex: int, result: Any) -> None:
+        pass
