ab869c6aa4c1a1997203e836b8464405f84295b9
Octavian Patrascoiu <opatrascoiu@yahoo.com>
2025-04-15 12:08:31 +0100

    Add CI files for unix

---
+++ ci_build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+export CI_PROJECT_DIR=.
+
+# mvn clean does not remove them
+rm -rf .pytest_cache
+rm -rf .tox
+rm -rf .venv
+
+source $CI_PROJECT_DIR/ci/make_env.sh .venv
+status=$?
+if [ $status -eq 0 ]
+then
+  echo "Python venv was created"
+else
+  echo "Cannot create Python venv"
+  exit 1
+fi
+
+pwd
+ls -la
+
+python -m pytest --cov=jdmn-python-runtime --junitxml=test-reports/test_report.xml
+status=$?
+if [ $status -eq 0 ]
+then
+  echo "pytest passed"
+else
+  echo "pytest failed"
+  exit 1
+fi
+python -m flake8
+status=$?
+if [ $status -eq 0 ]
+then
+  echo "flake8 passed"
+else
+  echo "flake8 failed"
+  exit 1
+fi
